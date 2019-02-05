31096c83301184586cab3166aa6ee010df85d803
Clara Bicalho <cbm343@nyu.edu>
2019-02-05 11:30:33 +0100

    Flexible inspector default argument
    Allows for flexible default inspector values (see #25).  It varies over the first argument of the chosen designer.
    
    - If the first argument is NULL, it varies over second argument;
    - If the first argument is N, if varies by 100 from default value, in increments of 10
    - If the first argument is not N, it varies based on min/max/step arguments in the definitions attribute of the chosen designer function.

--- tab_inspect.R
+++ tab_inspect.R
@@ -213,23 +213,18 @@ inspectTab <- function(input, output, session, design_tab_proxy) {
         })
         
         # set a default value for "N" the first time
-        # but there are some design without N argument 
-        d_args_charvec <- as.character(d_args)
-        names(d_args_charvec) <- names(d_args)
-        if (all(defaults == d_args_charvec) & !is.null(unlist(defaults['N']))) {
+        # but there are some design without N argument
+        first_arg <- names(d_args)[1]
+        if (first_arg == 'N' && is.null(d_args['N'])) first_arg <- names(d_args)[2]
+        if (first_arg == 'N') {
             n_int <- as.integer(defaults['N']) 
             defaults['N'] <- sprintf('%d, %d ... %d', n_int, n_int + 10, n_int + 100)
-        } else if (all(defaults == d_args_charvec) & !is.null(unlist(defaults['N_blocks']))){
-            min_int <- design_tab_proxy$react$design_argdefinitions[design_tab_proxy$react$design_argdefinitions$names == "N_blocks",]$inspector_min
-            max_int <- design_tab_proxy$react$design_argdefinitions[design_tab_proxy$react$design_argdefinitions$names == "N_blocks",]$inspector_max
-            step_int <- design_tab_proxy$react$design_argdefinitions[design_tab_proxy$react$design_argdefinitions$names == "N_blocks",]$inspector_step
-            defaults['N_blocks'] <- sprintf('%d, %d ... %d', min_int, min_int + step_int, max_int)
-            
-        } else if (all(defaults == d_args_charvec) & !is.null(unlist(defaults['N_groups']))){
-            min_int <- design_tab_proxy$react$design_argdefinitions[design_tab_proxy$react$design_argdefinitions$names == "N_groups",]$inspector_min
-            max_int <- design_tab_proxy$react$design_argdefinitions[design_tab_proxy$react$design_argdefinitions$names == "N_groups",]$inspector_max
-            step_int <- design_tab_proxy$react$design_argdefinitions[design_tab_proxy$react$design_argdefinitions$names == "N_groups",]$inspector_step
-            defaults['N_groups'] <- sprintf('%d, %d ... %d', min_int, min_int + step_int, max_int)
+        } else {
+            defs <- design_tab_proxy$react$design_argdefinitions
+            min_int <- defs$inspector_min[defs$names == first_arg]
+            max_int <- defs$inspector_max[defs$names == first_arg]
+            step_int <- defs$inspector_step[defs$names == first_arg]
+            defaults[first_arg] <- sprintf('%d, %d ... %d', min_int, min_int + step_int, max_int)
         }
         
         param_boxes <- create_design_parameter_ui('inspect', design_tab_proxy$react, NS('tab_inspect'),
