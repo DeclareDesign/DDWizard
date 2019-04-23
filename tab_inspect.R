0c21e9ab618932bed3bd841721b969d0385a4103
zerinahuang <huangsis@student.hu-berlin.de>
2019-04-23 15:06:36 +0200

    fix typo

--- tab_inspect.R
+++ tab_inspect.R
@@ -712,15 +712,7 @@ inspectTab <- function(input, output, session, design_tab_proxy) {
                         }else{
                             react$captured_errors <- paste('Please vary any following argument')
                         }
-                if (length(insp_args) > 0) {
-                    insp_args_NAs <- sapply(insp_args, function(arg) { any(is.na(arg)) })
-    
-                    if (sum(insp_args_NAs) > 0) {
-                        react$captured_errors <- paste('Invalid values supplied to the following arguments:',
-                                                       paste(names(insp_args_NAs)[insp_args_NAs], collapse = ', ')) 
-                        shinyjs::disable('update_plot')
-
-                    } else {
+                    }else{
                         react$captured_errors <- NULL
                         shinyjs::enable('update_plot')
                     }
