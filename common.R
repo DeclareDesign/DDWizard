dc1886ef3372b050fe9c188f2bfb41ff0afa6e07
Markus Konrad <markus.konrad@wzb.eu>
2019-06-03 14:01:22 +0200

    take argument value from design tab whenever it is changed there and use it in the inspect tab (closes #151)

--- common.R
+++ common.R
@@ -185,22 +185,42 @@ get_designer_args <- function(designer) {
     formals(designer)
 }
 
+# Take a argument value `arg_val` from a designer with class `arg_class` and `arg_is_vector` indicating if
+# the designer expects this argument to be a vector. Convert the argument to fractions for numbers with many repeating digits
+# (like 0.333... will become 1/3).
+# If `to_char` is TRUE, additionally convert the result to a character vector as formatted for an input box (e.g.
+# "(1/3, 1/3, 1/3)" for a vector input with fractions).
+designer_arg_value_to_fraction <- function(arg_val, arg_class, arg_is_vector, to_char = FALSE) {
+    # try to convert to fractions if there is a number with many repeating digits after the decimal point like "0.3333333333"
+    if (arg_class == "numeric" && any(grepl(sprintf('\\.(%s)$', paste(sprintf('%s{10,}', 1:9), collapse = '|')), as.character(arg_val)))) {
+        arg_val <- MASS::fractions(arg_val)
+    }
+    
+    if (to_char) {
+        arg_val <- as.character(arg_val)
+        
+        if (arg_is_vector) {  # vector of vectors input
+            return(sprintf('(%s)', paste(arg_val, collapse = ', ')))
+        } else {
+            return(arg_val)
+        }
+    } else {
+        arg_val
+    }
+}
+
+
 # evaluate argument defaults of designers in separate environment (because they might be "language" constructs)
 # return evaluated argument defaults
 # `args` is a list of arguments with defaults as returned from `formals(<designer>)`
 evaluate_designer_args <- function(args, definition) {
     eval_envir <- new.env()
     
-    args_eval <- lapply(1:length(args), function(a){
-        evaluated_arg <- invisible(eval(args[[a]], envir = eval_envir))
+    args_eval <- lapply(1:length(args), function(a) {
+        argdef <- definition[a,]
+        
         # convert the value to fraction if necessary
-        evaluated_arg_str <- as.character(args[[a]]) # e.g. c(1/3, 1/10) becomes character vector "c" "1/3" "1/10"
-        if (definition[a, "class"] == "numeric" && length(evaluated_arg_str) > 1
-            && evaluated_arg_str[1] == 'c'   # first element is c
-            && any(grepl('^\\d+/\\d+$', evaluated_arg_str[2:length(evaluated_arg_str)])))   # other elements must contain a fraction
-        {
-            evaluated_arg <- MASS::fractions(evaluated_arg)
-        }
+        evaluated_arg <- designer_arg_value_to_fraction(invisible(eval(args[[a]], envir = eval_envir)), argdef$class, argdef$vector)
         
         invisible(assign(x = names(args)[a], value = evaluated_arg, envir = eval_envir))
         hold <- invisible(get(names(args)[a], envir = eval_envir))
